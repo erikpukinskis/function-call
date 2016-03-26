c391f30bd2c791a3c5fe3867972cbb8bcf9497b1
Erik Pukinskis <erik@snowedin.net>
2016-03-26 16:38:53 -0700

    Split out of nrtv-browser-bridge, added functionCall.raw to add a raw javascript argument

---
+++ test.js
@@ -0,0 +1,81 @@
+var test = require("nrtv-test")(require)
+
+function greet(name) {
+  alert("hi, "+name)
+}
+
+test.using(
+  "raw arguments",
+  ["./"],
+  function(expect, done, functionCall) {
+    var call = functionCall(greet).withArgs(
+        "string",
+        functionCall.raw("event")
+      )
+
+    expect(call.evalable()).to.equal("greet(\"string\",event)")
+
+    done()
+  }
+)
+    
+test.using(
+  "arguments can be functions",
+
+  ["./"],
+  function(expect, done, functionCall) {
+
+    var takesACallback = functionCall(
+      function callIt(callback) {
+        callback()
+      }
+    )
+
+    var wit = takesACallback
+    .withArgs(function() {
+      alert("shabooya")
+    })
+
+    expect(wit.evalable()).to.match(/shabooya/)
+
+    done()
+  }
+)
+
+
+test.using(
+  "arguments can be objects",
+
+  ["./"],
+  function(expect, done, functionCall) {
+
+    var boundFunction = functionCall(function() {})
+
+    var source = boundFunction.withArgs({a: 2, b: "hello", c: [1,"hi"]}).evalable()
+
+    expect(source).to.contain("\"a\":2")
+
+    done()
+  }
+)
+
+
+test.using(
+  "arguments can be undefined",
+
+  ["./"],
+  function(expect, done, functionCall) {
+
+    var boundFunction = functionCall(greet)
+
+    var source = boundFunction.withArgs(undefined, {}).evalable()
+
+    expect(source).to.contain("undefined,{}")
+
+    done()
+  }
+)
+
+
+
+
